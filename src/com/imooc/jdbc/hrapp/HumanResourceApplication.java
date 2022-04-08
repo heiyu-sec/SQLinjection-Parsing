4721e4707ec0a1ecfee55d8c294cabe222efea35
heiyu <304429477@qq.com>
2022-04-08 11:31:14 +0800

    提交hrapp

---
+++ src/com/imooc/jdbc/hrapp/HumanResourceApplication.java
@@ -0,0 +1,18 @@
+package com.imooc.jdbc.hrapp;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
+public class HumanResourceApplication {
+    public static void main(String[] args) {
+        System.out.println("1-查询部门员工");
+        Scanner in = new Scanner(System.in);
+        Integer cmd = in.nextInt();
+        switch (cmd){
+            case 1: //查询部门员工
+
+                break;
+        }
+    }
+}
