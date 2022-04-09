5a294b1a9a146e77cb8d82a1e1261c4f9fafef05
heiyu <304429477@qq.com>
2022-04-09 17:25:39 +0800

    新增insert操作

--- src/com/imooc/jdbc/hrapp/HumanResourceApplication.java
+++ src/com/imooc/jdbc/hrapp/HumanResourceApplication.java
@@ -1,6 +1,7 @@
 package com.imooc.jdbc.hrapp;
 
 import com.imooc.jdbc.hrapp.command.Command;
+import com.imooc.jdbc.hrapp.command.InsertCommand;
 import com.imooc.jdbc.hrapp.command.PstmtQueryCommand;
 import com.imooc.jdbc.hrapp.command.QueryCommand;
 import org.w3c.dom.ls.LSInput;
@@ -10,11 +11,18 @@ import java.util.Scanner;
 public class HumanResourceApplication {
     public static void main(String[] args) {
         System.out.println("1-查询部门员工");
+        System.out.println("2-办理员工入职");
+        System.out.println("请选择功能");
         Scanner in = new Scanner(System.in);
         Integer cmd = in.nextInt();
+        Command command = null;
         switch (cmd){
             case 1: //查询部门员工
-                Command command = new PstmtQueryCommand();
+                command = new PstmtQueryCommand();
+                command.execute();
+                break;
+            case 2:
+                command = new InsertCommand();
                 command.execute();
                 break;
         }
